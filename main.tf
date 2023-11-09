e8f84d567eb8dc0d770a44b62b90ee1650f38eaa
shvarsha <130674453+shvarsha@users.noreply.github.com>
2023-11-09 17:36:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource null_resource "cli-test" {
     provisioner "local-exec" {
-    command = "pip3 install ibmcloudant==0.0.40"
+    ##command = "pip3 install ibmcloudant==0.0.40"
+    command = "python3 test.py"
     }
 }
