d7df2eb6c2c234cc29b336ac2d6abfff6b05dcb0
shvarsha <130674453+shvarsha@users.noreply.github.com>
2024-02-09 17:00:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource null_resource "cli-test" {
     provisioner "local-exec" {
     ##command = "pip3 install ibmcloudant==0.0.40"
-    command = "helm3 --help"
+    ##command = "helm3 --help"
     command = "go version"
     }
 }
