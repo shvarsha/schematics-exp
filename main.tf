5f2aa1c347cb4f795e47a86724ad1c37ceb4387d
shvarsha <130674453+shvarsha@users.noreply.github.com>
2024-04-01 12:34:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
 data "ibm_cm_catalog" "cm_catalog" {
 }
 
+output "catalog_list"{
+  valur = data.ibm_cm_catalog.cm_catalog
+}
+
 resource "ibm_cm_catalog" "cm_catalog" {
   label = "test-vpe-catalog"
   kind  = "vpe"
