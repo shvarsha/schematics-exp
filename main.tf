e61ee1ab39a4ef56396e351501d02c00b35d7858
shvarsha <130674453+shvarsha@users.noreply.github.com>
2024-06-10 16:11:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,5 @@
-data "ibm_cm_catalog" "cm_catalog" {
-}
-
-output "catalog_list"{
-  value = data.ibm_cm_catalog.cm_catalog
-}
-
-resource "ibm_cm_catalog" "cm_catalog" {
-  label = "test-vpe-catalog"
-  kind  = "vpe"
-  tags  = ["sdnlb"]
+resource null_resource "prov-test" {
+    provisioner "local-exec" {
+    command = "helm3 --help"
+    }
 }
